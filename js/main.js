c022e4da6cd3a49b75dfe9a60c94bafe360896f9
BinsonAniyan <binsonk18@gmail.com>
2021-11-28 00:33:43 +0530

    new repo

---
+++ js/main.js
@@ -0,0 +1,77 @@
+const inputs = document.querySelectorAll(".input");
+
+
+function addcl(){
+	let parent = this.parentNode.parentNode;
+	parent.classList.add("focus");
+}
+
+function remcl(){
+	let parent = this.parentNode.parentNode;
+	if(this.value == ""){
+		parent.classList.remove("focus");
+	}
+}
+
+
+inputs.forEach(input => {
+	input.addEventListener("focus", addcl);
+	input.addEventListener("blur", remcl);
+});
+
+
+function validate(redirect){
+	let name =document.getElementById("usen");
+    let pass =document.getElementById("passv");
+    if(name.value=='admin' && pass.value=='12345'){
+        
+		redirect();
+    }
+    else{
+        
+          alert("Ïnvalid Login Credentials!");  
+          
+        }
+    
+}
+function redirect(){
+	window.open("infff.html");
+}
+let count=0;
+  
+  function edit(obj) {
+
+
+    var id =obj.id
+    var label_id = 'label'+id;
+
+    if (obj.checked) {   
+
+       $("#"+id).prop("checked", true);
+
+
+        console.log(obj.id);
+  
+       count++;
+       console.log(count);
+       $( this ).attr( 'checked', true )
+
+
+       document.getElementById(label_id).style.textDecoration = 'line-through';
+       $("#"+id).prop("checked", true);
+
+
+       if (count == 5){
+       
+       alert('Congrats.  5 Tasks have been Successfully Completed');
+   }
+
+    } else {
+        // $( this ).attr( 'checked', false )
+      count--;
+      document.getElementById(label_id).style.textDecoration = '';
+    }
+  }
+
+
+
